4b6487da5177a5b6354655604f1fcea428b521b7
Łukasz Klekner <lukaszklekner@gmail.com>
2021-08-15 15:13:30 +0200

    creates LoginForm

---
+++ src/components/LoginForm.js
@@ -0,0 +1,46 @@
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import FormikControl from './FormikControl'
+
+const LoginForm = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  }
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().min(6, 'Password too short!').required('Required'),
+  })
+
+  const onSubmit = (values) => console.log('Form data', values)
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => (
+        <Form>
+          <FormikControl
+            control='input'
+            type='email'
+            label='Email'
+            name='email'
+          />
+          <FormikControl
+            control='input'
+            type='password'
+            label='Password'
+            name='password'
+          />
+          <button type='submit' disabled={!formik.isValid}>
+            Submit data!
+          </button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default LoginForm
