12718ddb00497a0e666e49e5955fdff33a5a8620
Łukasz Klekner <lukaszklekner@gmail.com>
2021-08-15 07:53:44 +0200

    adds FormikControl

---
+++ src/components/FormikControl.js
@@ -0,0 +1,14 @@
+const FormikControl = ({ control }) => {
+  switch (control) {
+    case 'input':
+    case 'textarea':
+    case 'select':
+    case 'radio':
+    case 'checkbox':
+    case 'date':
+    default:
+      return null
+  }
+}
+
+export default FormikControl
